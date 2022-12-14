6a4c421baa851d5408f217f45da37cdc316a769a
Valentynlang <vintenlang@gmail.com>
2022-12-14 02:21:34 +0100

    buttom test

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
 import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
 const tg = window.Telegram.WebApp;
 
 function App() {
+    const {onToggleButton, tg} = useTelegram()
 
     useEffect(() => {
         tg.ready()
     },[])
 
-    const onClose = () => {
-        tg.close()
-    }
   return (
     <div className="App">
-work
-        <button onClick={onClose}>End</button>
+        <button onClick={onToggleButton}>Toggle</button>
     </div>
   );
 }
